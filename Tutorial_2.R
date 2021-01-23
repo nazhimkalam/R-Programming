23acf11e359299c5565c14b096364cb491d8b68c
Nazhim <nazhimkalam@gmail.com>
2021-01-23 09:57:12 +0530

    vectors in R

---
+++ Tutorial_2.R
@@ -0,0 +1,51 @@
+# Data Types
+# The variables are assigned with R-objects and the type of the R-object becomes
+# the data types of the variable.
+
+# There are a number of R-Objects types:
+# - Vectors
+# - Lists
+# - Matrices
+# - Arrays
+# - Factors
+# - Data Frames
+
+# VECTORS 
+# (A vector is the simplest type of data structure in R.)
+# (A vector is a sequence of data elements of the same basic type)
+# Examples:
+
+v1 = "Nazhim Kalam" # 'character' vectors are strings
+print(class(v1))
+
+v2 = 10           # 'numeric' vectors are numbers (integers, decimals etc...)
+print(class(v2))
+
+v3 = 5.6
+print(class(v3))
+  
+v4 = TRUE        # 'logical' vectors are Booleans either (True or False)
+print(class(v4))
+
+# So far we were working with (Single Value Vector) now lets work on 
+# (Multiple value)
+
+apple = c('red', 'blue', 'green') # concatenate but can use as a list as well
+print(class(apple))
+print(apple[3])
+
+# LISTS
+
+
+# MATRICES
+
+
+# ARRAYS
+
+
+# FACTORS
+
+
+# DATA FRAMES
+
+ 
