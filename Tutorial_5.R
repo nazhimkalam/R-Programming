fd3a4a46c5d4b9c3af6b22632e02a334fff1bc9e
Nazhim <nazhimkalam@gmail.com>
2021-01-24 12:02:31 +0530

    creating histograms

--- Tutorial_5.R
+++ Tutorial_5.R
@@ -1,17 +1,5 @@
 customer
-y <- table(customer$Region)
-y
-
-View(y)
-
-barplot(y)
-barplot(y[order(y)])
-barplot(y[order(y)], horiz = TRUE)
-barplot(y[order(y)], horiz = TRUE, col = 'red')
-barplot(y[order(y)], horiz = TRUE, col = c('red', 'blue', 'yellow', 'green'))
-barplot(y[order(y)], horiz = TRUE, col = c('red', 'blue', 'yellow', 'green')
-        ,border = NA)
-barplot(y[order(y)], horiz = TRUE, col = c('red', 'blue', 'yellow', 'green')
-        ,border = NA, main = "Frequency of regions")
-barplot(y[order(y)], horiz = TRUE, col = c('red', 'blue', 'yellow', 'green')
-        ,border = NA, main = "Frequency of regions",xlab = "Number of Customers")
+View(customer)
+hist(customer$Age)
+hist(customer$Age, breaks = 5)
+hist(customer$Age, breaks = c(0, 40, 60, 100), freq = TRUE, col = "green")
