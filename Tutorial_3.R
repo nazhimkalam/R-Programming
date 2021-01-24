29e0babc726a8050bd64586ad4fbf513f7fa0a3c
Nazhim <nazhimkalam@gmail.com>
2021-01-24 11:28:12 +0530

    using scripts to install and import external packages

---
+++ Tutorial_3.R
@@ -0,0 +1,7 @@
+# installing packages using script, because when sharing codes with people,
+# there are also able to see what packages are installed and install them as
+# well.
+install.packages('LiblineaR')
+
+require("LiblineaR")
+ 
