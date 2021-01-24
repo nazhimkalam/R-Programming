c0a1b28329aa159556a14be341c4dd3329a34a45
Nazhim <nazhimkalam@gmail.com>
2021-01-24 11:42:21 +0530

    reading data from a csv and a txt file

--- Tutorial_4.R
+++ Tutorial_4.R
@@ -1,2 +1,4 @@
-# getting user inputs
-x <- scan()
+product <- read.table('GitHub/R-Programming/6.2 Product.txt', header = TRUE,
+                      sep = "\t") 
+
+customer <- read.csv('GitHub/R-Programming/6.1 Customer.csv')
